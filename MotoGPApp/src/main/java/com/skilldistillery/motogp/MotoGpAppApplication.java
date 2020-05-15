ad88ebbddc889922ecafa03e870d9e71e4d5d35d
Sergei Samoiloff <sergio.samoiloff@gmail.com>
2020-05-15 17:37:41 -0600

    Created MotoGPApp project

---
+++ MotoGPApp/src/main/java/com/skilldistillery/motogp/MotoGpAppApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.motogp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoGpAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoGpAppApplication.class, args);
+	}
+
+}
---
+++ MotoGPApp/src/test/java/com/skilldistillery/motogp/MotoGpAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.motogp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoGpAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
